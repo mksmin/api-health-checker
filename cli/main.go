340f3121e2d09b6960d46b06a7386b5414ad7d54
mks_min <mininname@vk.com>
2026-01-09 20:38:00 +0300

    feat: create checker-cli app

--- cli/main.go
+++ cli/main.go
@@ -1,3 +1,110 @@
-package note
+package main
 
-func main() {}
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type Service struct {
+	Name string `json:"Name"`
+	URL  string `json:"URL"`
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: checker-cli [list|add|delete]")
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+
+	serverAddr := os.Getenv("CHECKER_ADDR")
+	if serverAddr == "" {
+		serverAddr = "http://localhost:8080/services"
+	}
+
+	switch cmd {
+	case "list":
+		listServices(serverAddr)
+
+	case "add":
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		addName := addCmd.String("name", "", "Service name")
+		addURL := addCmd.String("url", "", "Service URL")
+
+		addCmd.Parse(os.Args[2:])
+		if *addName == "" || *addURL == "" {
+			fmt.Println("Usage: checker-cli add -name <name> -url <url>")
+			os.Exit(1)
+		}
+		addService(serverAddr, *addName, *addURL)
+	default:
+		fmt.Println("Unrecognized command: " + cmd)
+		os.Exit(1)
+	}
+}
+
+func listServices(
+	addr string,
+) {
+	resp, err := http.Get(addr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var raw interface{}
+	if err := json.Unmarshal(body, &raw); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	pretty, err := json.MarshalIndent(raw, "", "  ")
+	if err != nil {
+		fmt.Println("Error formatting JSON:", err)
+		return
+	}
+	fmt.Println(string(pretty))
+}
+
+func addService(
+	addr string,
+	name string,
+	url string,
+) {
+	s := Service{
+		Name: name,
+		URL:  url,
+	}
+	data, _ := json.Marshal(s)
+	resp, err := http.Post(
+		addr,
+		"application/json",
+		bytes.NewReader(data),
+	)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusCreated {
+		fmt.Println("Service added", name)
+	} else {
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println(
+			"Service failed",
+			resp.StatusCode,
+			string(body),
+		)
+	}
+
+}
